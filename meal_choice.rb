0319d830144231f971ed8c3a76f1c7bec74d7f5f
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-23 18:52:29 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice(meal)
-  if meal=0
+  if meal==0
     "meat"
   else
     "#{meal}"
